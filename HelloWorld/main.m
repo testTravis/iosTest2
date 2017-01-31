d326a9b55eb2431ff2aed15c716cd2169deeeb78
testTravis <erez.yehezkel@mobiletornado.com>
2017-01-31 17:03:57 +0200

    Update main.m

--- HelloWorld/main.m
+++ HelloWorld/main.m
@@ -10,8 +10,7 @@
 #import "AppDelegate.h"
 
 int main(int argc, char * argv[]) {
-    XCTAssertTrue(["8" isEqualToString:@"8"], @"Part 1 past - 8 is  8.");
-XCTAssertTrue(["8" isEqualToString:@"7"], @"Part 2 failed - 7 is not 8.");
+
     @autoreleasepool {
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
     }
