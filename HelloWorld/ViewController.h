a59dfd14cb930db549198fb0959cc0a1ee27cdbf
testTravis <erez.yehezkel@mobiletornado.com>
2017-01-31 14:58:56 +0200

    Add files via upload

---
+++ HelloWorld/ViewController.h
@@ -0,0 +1,17 @@
+//
+//  ViewController.h
+//  HelloWorld
+//
+//  Created by Anupam Chugh on 16/04/16.
+//  Copyright © 2016 Journal Dev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController{
+IBOutlet UILabel *label;
+}
+-(IBAction)showLabel;
+
+@end
+
